adc610f306b1f0d2cea933bcf834e228f639f0d1
loic <muruloic@gmail.com>
2019-10-27 01:23:53 -0400

    currentpython

--- server.py
+++ server.py
@@ -2,8 +2,7 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 from io import BytesIO
 
 
-HTMLtemplate = 
-"""
+HTMLtemplate =  """
 <!DOCTYPE html>
 <html>
     <head>
@@ -12,12 +11,12 @@ HTMLtemplate =
     </body>
 </html>
 """
+client = None
 
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.end_headers()
-        self.wfile.write(b'Hello, world!')  
 
     def do_POST(self):
         content_length = int(self.headers['Content-Length'])
@@ -28,8 +27,11 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         response.write(b'This is POST request. ')
         response.write(b'Received: ')
         response.write(body)
+        print(client)
         self.wfile.write(response.getvalue())
+        print(client)
+        #client.write(b'We can still talk')
 
 
-httpd = HTTPServer(('localhost', 8000), SimpleHTTPRequestHandler)
+httpd = HTTPServer(('', 8000), SimpleHTTPRequestHandler)
 httpd.serve_forever()
