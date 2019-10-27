c75e4d13bb6365f463a040540b915122d7dc0896
loic <muruloic@gmail.com>
2019-10-27 03:42:01 -0400

    make imports pretty

--- index.js
+++ index.js
@@ -1,31 +1,34 @@
 const express = require('express')
 const path = require('path');
-const app = express()
+
+const fs = require('fs');
+
+// Firebase
 const firebase = require("firebase/app");
 const db = require("firebase/database");
-const port = 3000
 
+let rawdata = fs.readFileSync('config.json');
+let CONFIG = JSON.parse(rawdata);
+
+// Launch express app
+const port = 3000
+const app = express()
 app.use(express.static("public"));
 
-// Set the configuration for your app
+// Firebase setup
 var config = {
   apiKey: "apiKey",
   authDomain: "geohealth-962d3.firebaseapp.com",
   databaseURL: "https://geohealth-962d3.firebaseio.com/",
   storageBucket: "bucket.appspot.com"
 };
-
 firebase.initializeApp(config);
-
-// Get a reference to the database service
 var database = firebase.database();
 
+app.listen(port, () => console.log(`App listening on port ${port}!`))
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/', 'index.html')));
+var starCountRef = firebase.database().ref('posts/');
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-
-var starCountRef = firebase.database().ref('posts/' + postId + '/starCount');
 starCountRef.on('value', function(snapshot) {
-  updateStarCount(postElement, snapshot.val());
+  // write here
 });
