932888a22f12777511fa61fbbcd44c9571e4159c
Setoville <a6seto@edu.uwaterloo.ca>
2019-10-27 03:34:13 -0400

    Add OpenWeather API capability

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
+
 const express = require('express')
 const path = require('path');
 const app = express()
 const firebase = require("firebase/app");
 const db = require("firebase/database");
-const port = 3000
+const port = 3000;
 
 app.use(express.static("public"));
 
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+var request = new XMLHttpRequest();
 // Set the configuration for your app
 var config = {
   apiKey: "apiKey",
@@ -25,7 +28,20 @@ app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/', 'index.html'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
-var starCountRef = firebase.database().ref('posts/' + postId + '/starCount');
-starCountRef.on('value', function(snapshot) {
-  updateStarCount(postElement, snapshot.val());
-});
+var starCountRef = firebase.database().ref('posts/starCount');
+// starCountRef.on('value', function(snapshot) {
+//   console.log(snapshot)
+  
+//   updateStarCount(postElement, snapshot.val());
+// });
+//var coord = {"lat":112.0, "long":39.0};
+params = [['lat',"30"],['lon',"136.1"],['APPID','app_id_key']]
+var url = new URL ("http://api.openweathermap.org/data/2.5/weather");
+url.search = new URLSearchParams(params).toString();
+
+request.open("GET", url)
+request.onreadystatechange = function() {
+  console.log(this.responseText);
+}
+
+request.send()
